105ce3cc9f819b8866d543583efc9134d2f77269
iForvert <iforvert@gmail.com>
2016-11-13 13:57:42 +0800

    Project BaseInfo Config

---
+++ FVSeatsPickerDemo/Classes/Others/FVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FVAppDelegate.h
+//  FVSeatsPicker
+//
+//  Created by iforvert on 2016/11/13.
+//  Copyright © 2016年 iforvert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FVAppDelegate : UIResponder<UIApplicationDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+
+@end
---
+++ FVSeatsPickerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FVSeatsPicker
+//
+//  Created by iforvert on 2016/11/13.
+//  Copyright © 2016年 iforvert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FVAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FVAppDelegate class]));
+    }
+}
